1769089d35c3a98e033272bb627f5230e2473bca
Vladimir Herrlein <vherrlein@gmail.com>
2021-02-16 15:02:39 +0100

    feat(decoder): Add process timing logs
    tags: develop

--- src/decoder.py
+++ src/decoder.py
@@ -2,7 +2,7 @@ from queue import Full
 import av
 import logging
 from SimpleQueue import SimpleQueue as Queue
-from datetime import datetime
+from datetime import datetime, time
 
 logger = logging.getLogger(__name__)
 
@@ -26,14 +26,25 @@ class Decoder:
         logger.info("Decoder deamon started")
         while self.running:
             try:
+                timing = datetime.now()
+
                 data = self.queue.get()
                 
+                time_acquire_lock = (datetime.now() - timing).total_seconds()
+                timing = datetime.now()
+
                 frames = [frame.to_image() for packet in self.codec.parse(data) if packet.is_corrupt == False for frame in self.codec.decode(packet) if frame.is_corrupt == False]
                 
+                time_parse_decode_frames = (datetime.now() - timing).total_seconds()
+                timing = datetime.now()
+
                 if frames != None and len(frames) > 0:
                     for frame_callback in self.frame_callbacks:
                         frame_callback(frames)
                     self.last_frame = frames[len(frames)-1]
+
+                time_dispatch_frames = (datetime.now() - timing).total_seconds()
+                logging.info("Process timing. Acquire Lock: %.4fs, Frame Decoding: %.4fs, Dispatch frames: %.4fs", time_acquire_lock, time_parse_decode_frames, time_dispatch_frames)
                         
             except Exception as ex:
                 logger.warning("Unexpected exception occured: %s, Traceback = ".format(str(ex)), exc_info=True)
