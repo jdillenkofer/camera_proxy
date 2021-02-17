bf1a20bf6056cc7ee4e631a44af92e4e6e296bf1
Vladimir Herrlein <vherrlein@gmail.com>
2021-02-17 09:39:59 +0100

    feat(decoder): refactor and optimize locks usages
    Refactor process stages (reading queue, process data and dispatch)
    Optimize reading queue process
    
    tags: ENHC-Optimizing_Process

--- src/decoder.py
+++ src/decoder.py
@@ -6,6 +6,10 @@ from datetime import datetime, time
 
 logger = logging.getLogger(__name__)
 
+def avg(list):
+    length = len(list)
+    return (sum(list)/length) if length > 0 else 0
+
 class Decoder:
     def __init__(self):
         self.codec = av.CodecContext.create('h264', 'r')
@@ -20,6 +24,16 @@ class Decoder:
     
     def remove_frame_callback(self, frame_callback):
         self.frame_callbacks.remove(frame_callback)
+
+    def _dispatch_frames(self, frames):
+        if frames == None or len(frames) == 0:
+            return
+        for frame_callback in self.frame_callbacks:
+            frame_callback(frames)
+        self.last_frame = frames[len(frames)-1]
+
+    def _parse_decode_binary(self, data):
+        return [frame.to_image() for packet in self.codec.parse(data) if packet.is_corrupt == False for frame in self.codec.decode(packet) if frame.is_corrupt == False]
     
     def process(self):
         self.running = True
@@ -29,22 +43,28 @@ class Decoder:
                 timing = datetime.now()
 
                 data = self.queue.get()
-                
+
+                if data == None or len(data) == 0:
+                    continue
+
                 time_acquire_lock = (datetime.now() - timing).total_seconds()
-                timing = datetime.now()
-
-                frames = [frame.to_image() for packet in self.codec.parse(data) if packet.is_corrupt == False for frame in self.codec.decode(packet) if frame.is_corrupt == False]
                 
-                time_parse_decode_frames = (datetime.now() - timing).total_seconds()
-                timing = datetime.now()
+                
+                timings=([], [])
 
-                if frames != None and len(frames) > 0:
-                    for frame_callback in self.frame_callbacks:
-                        frame_callback(frames)
-                    self.last_frame = frames[len(frames)-1]
+                while True:
+                    timing = datetime.now()
+                    frames = self._parse_decode_binary(data)
+                    timings[0].append((datetime.now() - timing).total_seconds())
+                    timing = datetime.now()
+                    self._dispatch_frames(frames)
+                    timings[1].append((datetime.now() - timing).total_seconds())
+                    if self.queue.empty() or data == None or len(data) == 0:
+                        break
+                    data = self.queue.get_nowait()
 
-                time_dispatch_frames = (datetime.now() - timing).total_seconds()
-                logger.info("Process timing. Acquire Lock: %.4fs, Frame Decoding: %.4fs, Dispatch frames: %.4fs", time_acquire_lock, time_parse_decode_frames, time_dispatch_frames)
+                    
+                logger.info("Process - Acquire Lock: %.4fs, Frame Decoding: %.4fs, Dispatch frames: %.4fs, Dequeue Count: %d", time_acquire_lock, avg(timings[0]), avg(timings[1]), len(timings[1]))
                         
             except Exception as ex:
                 logger.warning("Unexpected exception occured: %s, Traceback = ".format(str(ex)), exc_info=True)
@@ -69,4 +89,3 @@ class Decoder:
             self._log_queued_time()
         except Full:
             logger.info("Decoder queue size is FULL: %d", self.queue.qsize())
-
